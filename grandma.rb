612f02b9d4b9d13b05154de07bb68a26afbad01b
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-18 01:37:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,10 @@ def speak_to_grandma(phrase)
   elsif phrase.upcase == phrase 
     "NO, NOT SINCE 1938!"
   else 
-    ""
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
